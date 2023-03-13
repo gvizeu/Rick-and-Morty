09843ae51f5c00eeea2752321510f26cf078ce0f
Gonzalo vizeu <gonzalo.vizeu.pinheiro@gmail.com>
2023-03-13 11:03:24 +0000

    Initial Commit

---
+++ Rick and Morty/Rick_and_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_MortyApp.swift
+//  Rick and Morty
+//
+//  Created by Gonzalo Vizeu on 13/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
